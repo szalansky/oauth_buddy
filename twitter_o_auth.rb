32cd01afca0a1de8c160d98496852263b28609b2
szalansky <rafal.szalanski@gmail.com>
2013-02-22 11:54:39 +0100

    Generating headers

---
+++ twitter_o_auth.rb
@@ -0,0 +1,72 @@
+require 'cgi'
+require 'uri'
+require 'base64'
+require 'openssl'
+require 'time'
+
+class TwitterOAuth
+  def initialize consumer_key, consumer_secret, token, token_secret,
+                method='GET', signature_method='HMAC-SHA1',
+                base_url = 'https://api.twitter.com/1.1/search/tweets.json',
+                version = '1.0', timezone = 'PST'
+    @consumer_key = consumer_key
+    @consumer_secret = consumer_secret
+    @token = token
+    @token_secret = token_secret
+    @method = method.upcase
+    @signature_method = signature_method
+    @base_url = base_url
+    @version = version
+    @timezone = timezone
+  end
+
+  def authorization_header query_string
+    nonce = random_nonce
+    timestamp = current_timestamp
+    signature = signature query_string, nonce, timestamp
+    [
+      "Authorization: OAuth oauth_consumer_key=\"#{@consumer_key}\"",
+      "oauth_nonce=\"#{nonce}\"", "oauth_signature=\"#{ percent_encode(signature) }\"",
+      "oauth_signature_method=\"#{@signature_method}\"", "oauth_timestamp=\"#{timestamp}\"",
+      "oauth_token=\"#{@token}\"",
+      "oauth_version=\"#{@version}\""
+    ].join(', ')
+  end
+
+  def signature query_string, nonce, timestamp
+    base_string = signature_base_string query_string, nonce, timestamp
+    key = "#{ percent_encode(@consumer_secret) }&#{ percent_encode(@token_secret) }"
+    digest = OpenSSL::Digest::Digest.new('sha1')
+    hmac = OpenSSL::HMAC.digest(digest, key, base_string)
+    Base64.encode64(hmac).chomp.gsub(/\n/, '')
+  end
+
+  def signature_base_string query_string, nonce, timestamp
+    sorted_query = sort_query [query_string, "oauth_consumer_key=#{@consumer_key}",
+                               "oauth_nonce=#{nonce}", "oauth_signature_method=#{@signature_method}",
+                               "oauth_timestamp=#{timestamp}",
+                               "oauth_token=#{@token}", "oauth_version=#{@version}"].join('&')
+    "#{@method}&#{ percent_encode @base_url }&#{ percent_encode urlize(sorted_query) }"
+  end
+
+  def random_nonce
+    Array.new( 32 ) { rand(256) }.pack('C*').unpack('H*').first
+  end
+
+  def percent_encode string
+    CGI.escape(string)
+  end
+
+  def sort_query query_string
+    CGI.parse(query_string).sort.map { |k, v| "#{ k }=#{ v.first.gsub(' ', '+') }" }.join('&')
+  end
+
+  def urlize query_string
+    query_string.gsub('+', '%20')
+  end
+
+  def current_timestamp
+    ((Time.now.to_f - Time.zone_offset(@timezone))).to_i
+  end
+end
+
---
+++ twitter_o_auth_spec.rb
@@ -0,0 +1,43 @@
+require "rspec"
+require_relative 'twitter_o_auth'
+
+describe "TwitterOAuth behaviour" do
+
+  before(:each) do
+    @oauth = TwitterOAuth.new("82fFtDtz3IQj5Hc7M1whBQ", 'XtcjPlTeVo8ZroQOlQLUJFMYbCz0eipOAZIAmatvkpw',
+                              '385960709-gYAi6Ld5K7fCoxFVDpjXVXBnDTZ25RrCs94g64pT',
+                              'sUEUWMaj5EhXqS1QOjvCbqIecJpAqu3da3oy8Cinc')
+
+    @oauth.stub(:random_nonce) { "f84a26af9d28928183c5907b9f274389" }
+    @oauth.stub(:current_timestamp) { "1360838236" }
+    @nonce = @oauth.random_nonce
+    @timestamp = @oauth.current_timestamp
+    @parameter_string = 'q=apple+macbook+pro&count=5'
+  end
+
+  it "returns query with sorted params" do
+    @oauth.sort_query(@parameter_string).should eq('count=5&q=apple+macbook+pro')
+  end
+
+  it "returns signature base string" do
+    signature_base_string = 'GET&https%3A%2F%2Fapi.twitter.com%2F1.1%2Fsearch%2Ftweets.json&count%3D5%26oauth_consumer_key%3D82fFtDtz3IQj5Hc7M1whBQ%26oauth_nonce%3Df84a26af9d28928183c5907b9f274389%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1360838236%26oauth_token%3D385960709-gYAi6Ld5K7fCoxFVDpjXVXBnDTZ25RrCs94g64pT%26oauth_version%3D1.0%26q%3Dapple%2520macbook%2520pro'
+    @oauth.signature_base_string(@parameter_string, @nonce, @timestamp).should eq(signature_base_string)
+  end
+
+  it "returns signature" do
+    @oauth.signature(@parameter_string, @nonce, @timestamp).should eq("fnhGlJURxyFykLqZKc9sIjn440E=")
+  end
+
+  it "returns authorization header" do
+    header = 'Authorization: OAuth oauth_consumer_key="82fFtDtz3IQj5Hc7M1whBQ", oauth_nonce="f84a26af9d28928183c5907b9f274389", oauth_signature="fnhGlJURxyFykLqZKc9sIjn440E%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1360838236", oauth_token="385960709-gYAi6Ld5K7fCoxFVDpjXVXBnDTZ25RrCs94g64pT", oauth_version="1.0"'
+    @oauth.authorization_header(@parameter_string).should eq(header)
+  end
+
+  it "returns random nonce" do
+    @oauth.random_nonce.should eq("f84a26af9d28928183c5907b9f274389")
+  end
+
+  it "returns current timestamp" do
+    @oauth.current_timestamp.should eq("1360838236")
+  end
+end
